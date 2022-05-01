51224f97999439f5312b69d209382a85c1f3c948
orjocagre <orlandocaceres114@gmail.com>
2022-04-30 19:39:38 -0600

    first commit

---
+++ aplicacion.js
@@ -0,0 +1,8 @@
+function calcular(){
+
+    let sbm = document.getElementById("salario").value;
+    let di = sbm * 0.07;
+
+    document.getElementById("sbm").value=sbm;
+    document.getElementById("di").value=di;
+}
